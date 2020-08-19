18856764bd0e3b300b53ea311d7691afb1800034
Heath Mast <hrmast@gmail.com>
2020-08-19 03:00:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
         @status = "rejected"
         return "Transaction rejected. Please check your account balance."
       end
-#binding.pry
+binding.pry
     end
 
     def reverse_transfer
